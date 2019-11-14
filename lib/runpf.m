b3abac52c9ed94122bcbcf747d6c352c82cc6c16
ELSYS\dgrant37 <daniel.grant@gtri.gatech.edu>
2019-11-14 18:37:19 -0500

    added option to only use PV bus voltages

--- lib/runpf.m
+++ lib/runpf.m
@@ -166,6 +166,26 @@ if ~isempty(mpc.bus)
             % [ B21 | B22 | ... ] -> [ V2*V1*B21 | V2*V2*B22 | ... ]
             % [ ... | ... | ... ]    [    ...    |    ...    | ... ]
             B = V_cols.*V_rows.*B;
+        elseif mptop.pf.dc.Bm == 2
+            V = abs(mpc.bus(:,VM).*exp(1i*mpc.bus(:,VA)*pi/180)); % Complex voltage phasor
+            % Cold start option: use only PV bus voltages
+            for i = 1:size(V)
+                if any(pv==i)||any(pq==i)
+                    V(i) = 1;
+                end
+            end
+            n = size(V, 1);
+            V_cols = zeros(n,n);
+            V_rows = zeros(n,n);
+            for i = 1:n
+                V_cols(:,i) = V;
+                V_rows(i,:) = V;
+            end
+            % Modify the B matrix using the Voltage magnitudes
+            % [ B11 | B12 | ... ]    [ V1*V1*B11 | V1*V2*B12 | ... ]
+            % [ B21 | B22 | ... ] -> [ V2*V1*B21 | V2*V2*B22 | ... ]
+            % [ ... | ... | ... ]    [    ...    |    ...    | ... ]
+            B = V_cols.*V_rows.*B;            
         else
             V = ones(size(bus, 1), 1);
         end
